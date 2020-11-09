5166a879495045fe20056aeef064a6893366ee16
Uros <49-2018@ftn.edu.rs>
2020-11-09 19:33:05 +0100

    Kreiranje klase ExerciseResult

---
+++ Exercise_49-2018/Exercise_49-2018/ExerciseResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_49_2018
+{
+    class ExerciseResult
+    {
+        private static int idd = 0;
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Point { get; set; }
+
+        public ExerciseResult(string name, string index, int point)
+        {
+            Id = idd++;
+            StudentName = name;
+            StudentIndex = index;
+            Point = point;
+        }
+        public ExerciseResult() { }
+    }
+}

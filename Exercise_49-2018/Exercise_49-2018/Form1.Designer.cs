81090b339544fc3e27eb6d053110af271696c711
Uros <49-2018@ftn.edu.rs>
2020-11-09 19:57:21 +0100

    Kreiranje listBox-a kao i ispisivanje podataka iz tabele

--- Exercise_49-2018/Exercise_49-2018/Form1.Designer.cs
+++ Exercise_49-2018/Exercise_49-2018/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(490, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(239, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Exercise_49-2018/Exercise_49-2018/Form1.cs
+++ Exercise_49-2018/Exercise_49-2018/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Exercise_49_2018
 {
@@ -17,5 +18,46 @@ namespace Exercise_49_2018
             InitializeComponent();
         }
 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<ExerciseResult> lista = new List<ExerciseResult>();
+            using (SqlConnection konekcija = new SqlConnection())
+            {
+                konekcija.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                konekcija.Open();
+                string q = "select* from ExerciseResults";
+
+                SqlCommand sqlCommand = new SqlCommand(q, konekcija);
+                using (SqlDataReader citac = sqlCommand.ExecuteReader())
+                {
+
+
+                    while (citac.Read())
+                    {
+                        ExerciseResult exerciseResult = new ExerciseResult();
+                        exerciseResult.Id = (int)citac.GetInt32(0);
+                        exerciseResult.StudentName = citac.GetString(1);
+                        exerciseResult.StudentIndex = citac.GetString(2);
+                        exerciseResult.Point = (int)citac.GetInt32(3);
+                        lista.Add(exerciseResult);
+                    }
+
+
+                }
+
+
+            }
+            listBox1.Items.Clear();
+            foreach (ExerciseResult exerciseResult1 in lista)
+            {
+                listBox1.Items.Add(exerciseResult1.StudentName + " " +
+                    exerciseResult1.StudentIndex + " " + exerciseResult1.Point);
+            }
+        }
     }
 }
